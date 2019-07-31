37ce5a7bfc3e07fcdd5f67f28ee7d939a3a1256e
Craily <Craily@Cyc-Home>
2019-07-31 21:12:22 +0800

    添加占位文件，保持目录结构，防止Git不上传空文件夹

---
+++ src/main/java/com/craily/DemoInterceptor.java
@@ -0,0 +1,31 @@
+package com.craily;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class DemoInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO 在请求发生前执行
+		long startTime = System.currentTimeMillis();
+		request.setAttribute("startTime", startTime);
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO 在请求完成后执行
+		long startTime = (long) request.getAttribute("startTime");
+		request.removeAttribute("startTime");
+		long endTime = System.currentTimeMillis();
+		System.out.println("本次请求处理时间为：" + new Long(endTime - startTime) + "ms");
+		request.setAttribute("handlingTime", endTime - startTime);
+	}
+
+}
--- src/main/java/com/craily/MyMvcConfig.java
+++ src/main/java/com/craily/MyMvcConfig.java
@@ -4,20 +4,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.craily")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter{
 	
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
+		// TODO 配置视图渲染器
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setPrefix("/WEB-INF/classes/view/");
 		viewResolver.setSuffix(".jsp");
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
 	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO 配置静态资源
+		/**
+		 * 指的是文件放置的目录，addResourceHandler指的是对外暴露的访问路径
+		 */
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+	}
+	
+	/**
+	 * 配置拦截器的Bean
+	 */
+	@Bean
+	public DemoInterceptor demoInterceptor() {
+		return new DemoInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO 注册拦截器
+		registry.addInterceptor(demoInterceptor());
+	}
+	
+	
 }
